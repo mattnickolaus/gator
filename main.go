9b5aad0043c139ef53fe0fd6d210ad3a87301576
mattnickolaus <mattnickolaus@gmail.com>
2025-09-30 21:37:43 -0400

    feat: setting up api for command arguments

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/mattnickolaus/gator/internal/config"
 )
@@ -9,17 +10,40 @@ import (
 func main() {
 	c, err := config.Read()
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
 	}
 
-	err = c.SetUser("mattn")
+	s := config.InitState(&c)
+	cmds := config.InitCommands()
+	cmds.Register("login", config.HandlerLogins)
+
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("Error: Not enough arguments were provided\n")
+		os.Exit(1)
+	}
+	// args[0] is the program name so ignore
+	cmdName := args[1]
+	var cmdArgs []string
+	if len(args) > 2 {
+		cmdArgs = args[2:]
+	}
+	cmd := config.Command{
+		Name: cmdName,
+		Args: cmdArgs,
+	}
+
+	err = cmds.Run(s, cmd)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
 	}
 
 	c, err = config.Read()
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
 	}
 	fmt.Printf("Config:\n %+v\n", c)
 }
